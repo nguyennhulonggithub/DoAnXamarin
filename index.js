eb2d280bf39de1875cd8dea0136c91c51986a529
vamirio256 <nguyenquocquan.nbk.9a5@gmail.com>
2021-12-21 13:05:26 +0700

    check only branch

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 app.use(
-  cors({
-    origin: "*",
-  })
+    cors({
+        origin: "*",
+    })
 );
 //test
 app.use(express.json());
-//test2
+
 app.use("/manga", express.static("manga"));
 
 const postUser = require("./routes/User");
 app.use("/users", postUser);
 
 app.listen(3000, () => {
-  console.log("server is running on port 3000");
+    console.log("server is running on port 3000");
 });
