d2373c825732cc7e8d568c206ed6129a1a3a99db
nguyennhulong9d1@gmail.com <nguyennhulong9d1@gmail.com>
2021-12-28 23:25:49 +0700

    new

--- screen/ChapterScreen.js
+++ screen/ChapterScreen.js
@@ -1,10 +1,55 @@
+import axios from "axios";
 import React from "react";
-import { View, Text, Image, ScrollView } from "react-native";
+
+import { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  FlatList,
+  Image,
+  Text,
+  useWindowDimensions,
+  Dimensions,
+  ScrollView,
+  Pressable,
+  TouchableWithoutFeedback,
+} from "react-native";
+
+import { server } from "../variable/ServerName";
 
 export default function ChapterScreen() {
+  const [data, setdata] = useState([]);
+  const windowWidth = Dimensions.get("window").width;
+
+  useEffect(() => {
+    axios.get(server + "/chapter/10").then((res) => setdata(res.data));
+    return;
+  }, []);
+
+  const renderItem = ({ item }) => {
+    return (
+      <TouchableWithoutFeedback
+        onPress={(evt) => console.log(evt.nativeEvent.locationY)}
+      >
+        <Image
+          source={{ uri: server + item.imgUrl }}
+          style={{
+            width: windowWidth,
+            height: (windowWidth * item.height) / item.width,
+          }}
+          fadeDuration={0}
+        />
+      </TouchableWithoutFeedback>
+    );
+  };
+
   return (
-    <ScrollView>
-      <Text>Hello</Text>
-    </ScrollView>
+    <FlatList
+      showsVerticalScrollIndicator={false}
+      data={data}
+      keyExtractor={(item, index) => item.key}
+      renderItem={renderItem}
+      onScroll={(e) => console.log(e.nativeEvent.contentOffset.y)}
+      maxToRenderPerBatch={60} // Reduce number in each render batch
+    />
   );
 }
