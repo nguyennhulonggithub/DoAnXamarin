8cae7ba85753cc8691ac5f749fec2f8c2a3d2039
nguyennhulong9d1@gmail.com <nguyennhulong9d1@gmail.com>
2021-12-28 15:15:50 +0700

    test

--- screen/ChapterScreen.js
+++ screen/ChapterScreen.js
@@ -1,9 +1,5 @@
 import React from "react";
 import { View, Text, Image, ScrollView } from "react-native";
-const data = [];
-for (let i = 0; i < 50; i++) {
-  data.push("../assets/testChap/" + i + ".jpg");
-}
 
 export default function ChapterScreen() {
   return (
