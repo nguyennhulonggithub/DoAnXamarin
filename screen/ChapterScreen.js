a48ccf1b3ff30c204f6b99aec4ab8de6abb9a711
nguyennhulong9d1@gmail.com <nguyennhulong9d1@gmail.com>
2021-12-29 11:34:07 +0700

    update chapter

--- screen/ChapterScreen.js
+++ screen/ChapterScreen.js
@@ -1,55 +1,87 @@
 import axios from "axios";
 import React from "react";
+import { useRef } from "react";
 
 import { useState, useEffect } from "react";
 import {
-  StyleSheet,
   FlatList,
   Image,
   Text,
-  useWindowDimensions,
   Dimensions,
-  ScrollView,
-  Pressable,
   TouchableWithoutFeedback,
+  View,
+  StyleSheet,
 } from "react-native";
+import { Color } from "../variable/Color";
 
 import { server } from "../variable/ServerName";
 
 export default function ChapterScreen() {
   const [data, setdata] = useState([]);
   const windowWidth = Dimensions.get("window").width;
-
+  const scrollItem = useRef();
+  const [itemHeights, set_itemHeights] = useState([]);
   useEffect(() => {
-    axios.get(server + "/chapter/10").then((res) => setdata(res.data));
+    axios.get(server + "/chapter/10").then((res) => {
+      setdata(res.data);
+      let array = [];
+      res.data.forEach((element) => {
+        array.push(
+          Math.floor((windowWidth * element.height) / element.width) + 15
+        );
+      });
+      set_itemHeights(array);
+    });
     return;
   }, []);
 
-  const renderItem = ({ item }) => {
+  const renderItem = ({ item, index }) => {
     return (
       <TouchableWithoutFeedback
-        onPress={(evt) => console.log(evt.nativeEvent.locationY)}
+        onPress={() => {
+          scrollItem.current.scrollToIndex({
+            index: index - 1,
+            viewPosition: 0.5,
+          });
+        }}
       >
         <Image
           source={{ uri: server + item.imgUrl }}
           style={{
             width: windowWidth,
-            height: (windowWidth * item.height) / item.width,
+            height: itemHeights[index] - 15,
           }}
           fadeDuration={0}
         />
       </TouchableWithoutFeedback>
     );
   };
+  const renderSeparator = () => <View style={styles.separator} />;
+  const getItemLayout = (data, index) => {
+    let length = itemHeights[index];
+    const offset = itemHeights.slice(0, index).reduce((a, c) => a + c, 0);
+    if (!length) {
+      length = 0;
+    }
+    return { length, offset, index };
+  };
 
   return (
     <FlatList
+      ItemSeparatorComponent={renderSeparator}
       showsVerticalScrollIndicator={false}
+      ref={scrollItem}
       data={data}
       keyExtractor={(item, index) => item.key}
       renderItem={renderItem}
-      onScroll={(e) => console.log(e.nativeEvent.contentOffset.y)}
+      getItemLayout={getItemLayout}
       maxToRenderPerBatch={60} // Reduce number in each render batch
     />
   );
 }
+const styles = StyleSheet.create({
+  separator: {
+    backgroundColor: Color.defaultColor,
+    height: 15,
+  },
+});
--- screen/Test.js
+++ screen/Test.js
@@ -1,10 +1,131 @@
 import React from "react";
-import { View, Text } from "react-native";
+import {
+  SafeAreaView,
+  View,
+  FlatList,
+  StyleSheet,
+  Text,
+  Button,
+} from "react-native";
 
-export default function Test() {
-  return (
-    <View>
-      <View style={{ backgroundColor: "red", height: 500, width: 500 }}></View>
-    </View>
-  );
+const DATA = [
+  {
+    id: "0",
+    title: "Zero",
+  },
+  {
+    id: "1",
+    title: "One",
+  },
+  {
+    id: "2",
+    title: "Two",
+  },
+  {
+    id: "3",
+    title: "Three",
+  },
+  {
+    id: "4",
+    title: "Four",
+  },
+  {
+    id: "5",
+    title: "Five",
+  },
+  {
+    id: "6",
+    title: "Six",
+  },
+  {
+    id: "7",
+    title: "Seven",
+  },
+  {
+    id: "8",
+    title: "Eight",
+  },
+  {
+    id: "9",
+    title: "Nine",
+  },
+  {
+    id: "10",
+    title: "Ten",
+  },
+];
+
+export default class Test extends React.Component {
+  flatListRef;
+
+  constructor(props) {
+    super(props);
+    this.itemHeights = [];
+  }
+
+  // componentDidMount() {
+  //   setTimeout(() => {
+  //     this.flatListRef.scrollToIndex({
+  //       animate: true,
+  //       index: 6,
+  //     });
+  //   }, 300);
+  // }
+  getItemLayout = (data, index) => {
+    let length = this.itemHeights[index];
+    const offset = this.itemHeights.slice(0, index).reduce((a, c) => a + c, 0);
+
+    if (!length) {
+      length = 0;
+    }
+    return { length, offset, index };
+  };
+
+  rendeItem = ({ item, index }) => {
+    const randomHeight = Math.floor(Math.random() * 250) + 50;
+    return (
+      <View
+        style={[styles.item, { height: randomHeight }]}
+        onLayout={(object) =>
+          (this.itemHeights[index] = object.nativeEvent.layout.height)
+        }
+      >
+        <Text style={styles.title}>{item.title}</Text>
+      </View>
+    );
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <FlatList
+          ref={(ref) => {
+            this.flatListRef = ref;
+          }}
+          data={DATA}
+          renderItem={this.rendeItem}
+          keyExtractor={(item) => item.id}
+          getItemLayout={this.getItemLayout}
+          initialNumToRender={1}
+          maxToRenderPerBatch={1}
+        />
+      </View>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+
+    width: "100%",
+  },
+  item: {
+    backgroundColor: "#f9c2ff",
+    borderWidth: 1,
+    width: "100%",
+  },
+  title: {
+    fontSize: 32,
+  },
+});
