05c7b0d5fcb4fe3502cd0ae1ce732b291600fb46
nguyennhulong9d1@gmail.com <nguyennhulong9d1@gmail.com>
2022-01-09 10:00:51 +0700

    new

--- routes/Search.js
+++ routes/Search.js
@@ -2,20 +2,23 @@ const express = require("express");
 const router = express.Router();
 const sqlConnection = require("./sqlConnection");
 
-router.get('/:word', (req, res) => {
-    let word = req.params.word;
-    let sql = "select idManga, Name, ImageAPI, Free, (select count(*) from chapter where manga_idManga = idManga)as Chapter from manga where Name like '%" + word + "%';";
-    sqlConnection(sql, (err, results) => {
-        if (err) {
-            res.send(err);
-        } else {
-            if (results.length == 0) {
-                res.send(req.params.word + ' not exist');
-            } else {
-                res.send(results);
-            }
-        }
-    })
-})
+router.get("/:word", (req, res) => {
+  let word = req.params.word;
+  let sql =
+    "select idManga, Name, ImageAPI, Free, (select count(*) from chapter where manga_idManga = idManga)as Chapter from manga where Name like '%" +
+    word +
+    "%';";
+  sqlConnection(sql, (err, results) => {
+    if (err) {
+      res.send(err);
+    } else {
+      if (results.length == 0) {
+        res.send(null);
+      } else {
+        res.send(null);
+      }
+    }
+  });
+});
 
 module.exports = router;
