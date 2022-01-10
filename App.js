b7bbbcdc2a2fd44bc31be44fa941b78e3b48eed3
nguyennhulong9d1@gmail.com <nguyennhulong9d1@gmail.com>
2022-01-10 09:19:37 +0700

    new

--- App.js
+++ App.js
@@ -9,23 +9,23 @@ import { createStore } from "redux";
 import { Provider } from "react-redux";
 import allReducer from "./redux/reducers";
 import { composeWithDevTools } from "redux-devtools-extension";
-// import { LogBox } from "react-native";
-// LogBox.ignoreLogs(["Warning: ..."]); // Ignore log notification by message
-// LogBox.ignoreAllLogs(); //Ignore all log notifications
+import { LogBox } from "react-native";
+LogBox.ignoreLogs(["Warning: ..."]); // Ignore log notification by message
+LogBox.ignoreAllLogs(); //Ignore all log notifications
 
 //test clone branch
 const store = createStore(allReducer, composeWithDevTools());
 //checkout
 
 export default class App extends Component {
-    render() {
-        return (
-            <Provider store={store}>
-                <NavigationContainer>
-                    <TabNavigation />
-                </NavigationContainer>
-            </Provider>
-            // <Test />
-        );
-    }
+  render() {
+    return (
+      <Provider store={store}>
+        <NavigationContainer>
+          <TabNavigation />
+        </NavigationContainer>
+      </Provider>
+      // <Test />
+    );
+  }
 }
